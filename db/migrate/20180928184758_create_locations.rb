3d7b6fe289f957d38b90e8df406e71230315e1c4
FKRauber <fabienne.rauber@gmail.com>
2018-09-28 17:21:41 -0400

    updated attribute name from year_completed to year_visited and built users/show

--- db/migrate/20180928184758_create_locations.rb
+++ db/migrate/20180928184758_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration
   def change
   	create_table :locations do |t|
       t.string :name
-      t.integer :year_completed
+      t.integer :year_visited
     end
   end
 end
