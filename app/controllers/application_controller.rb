70168b43f97149513687d75ecd53789898c69fd2
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-09-29 22:59:22 +0000

    updated appcontroller to require environment.rb and ran migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
-  set :views, proc { File.join(root, '../views/') }
-  register Sinatra::Twitter::Bootstrap::Assets
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'gonetoaplace'
+  end
 
   get '/' do
     erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 
 end
