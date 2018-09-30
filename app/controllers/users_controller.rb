4df22ec833a50e60c12a2043a74e2f2e98e99201
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-09-30 22:50:01 +0000

    updated paths and completed post'/login' and post'/signup'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,18 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'/users/new', locals: {message: "Please Sign Up before you Sign In"}
     else
-      redirect to '/show'
+      redirect to '/users/show'
     end
   end
   post '/signup' do
-
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/users/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/users/show'
+    end
   end
 
   # /LOGIN
@@ -21,18 +28,24 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/login'
     else
-      redirect to '/show'
+      redirect to 'users/show'
     end
   end
   post '/login' do
-
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/users/show'
+    else
+      redirect to '/users/signup'
+    end
   end
 
   # LOGOUT
   get '/logout' do
     if logged_in?
       session.destroy
-      redirect to '/login'
+      redirect to '/users/login'
     else
       redirect to '/'
     end
