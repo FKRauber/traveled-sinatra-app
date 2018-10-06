96a405ca874575ec136a9a025dca2c1f6d91b69f
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-06 01:13:39 +0000

    fixed base routes ; set up basic locations/edit and /new ; added breaks in locations/show

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,14 +7,14 @@ class LocationsController < ApplicationController
   get '/locations/show' do
     erb :'/locations/show'
   end
-  #
-  # get '/new' do
-  #   erb :'/locations/new'
-  # end
-  #
-  # get '/edit' do
-  #   erb :'/locations/edit'
-  # end
+
+  get '/locations/new' do
+    erb :'/locations/new'
+  end
+
+  get '/locations/edit' do
+    erb :'/locations/edit'
+  end
 
   delete '/locations/:id/delete' do
     @location = Location.find_by_id(params[:id])
